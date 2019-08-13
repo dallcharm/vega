396d67aa9a7f909cc8e21fbb9adcd0b3fee22c13
dallcharm <dallcharm@gmail.com>
2019-08-12 21:32:56 -0500

    Updates

---
+++ precache-manifest.67c2e6cab256fb5b35f28e9a28f53489.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33110dfe499c643aaeedd541a1e73141",
+    "url": "/vega/index.html"
+  },
+  {
+    "revision": "bb7bbb945c7f123c28f2",
+    "url": "/vega/static/css/main.2049d31a.chunk.css"
+  },
+  {
+    "revision": "94ac0f2759da5d9ff6bf",
+    "url": "/vega/static/js/2.9b79248a.chunk.js"
+  },
+  {
+    "revision": "bb7bbb945c7f123c28f2",
+    "url": "/vega/static/js/main.ac656528.chunk.js"
+  },
+  {
+    "revision": "77f618274e7845fb776c",
+    "url": "/vega/static/js/runtime~main.4df2cea0.js"
+  },
+  {
+    "revision": "4ff4a6e04489b572e297725522176d27",
+    "url": "/vega/static/media/scheduling.4ff4a6e0.png"
+  }
+]);
